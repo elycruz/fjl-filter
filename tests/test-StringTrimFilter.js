ced1deac03c9dbd5884d4693fa8efde2e92094b9
Ely De La Cruz <elycruz@elycruz.com>
2018-01-07 00:09:17 -0500

    xml-entities-filter.

--- tests/test-StringTrimFilter.js
+++ tests/test-StringTrimFilter.js
@@ -4,7 +4,10 @@ import {expect} from 'chai';
 describe ('StringTrimFilter', function () {
     describe ('#stringTrim', function () {
         test ('should trim spaces from both front and end of a string', function () {
-            expect(stringTrim(' hello world ')).to.equal('hello-world');
+            expect(stringTrim(' hello world ')).to.equal('hello world');
+        });
+        test ('should return empty string when receiving empty string', function () {
+            expect(stringTrim('')).to.equal('');
         });
     });
 });
